0e8db40937a0742bd1f03058c330048d78eac597
Tadao Isobe <a1103ti@aiit.ac.jp>
2012-12-15 10:37:59 +0900

    second commit

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{creami:0,search:0,welcom:0,index:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Creamy Documents&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{"creamy\u3068\u306f":0,creami:0,search:0,index:0,modul:0,indic:0,tabl:0,"\u3078\u3088\u3046\u3053\u305d":0,page:0},objtypes:{},titles:["Creamy \u3078\u3088\u3046\u3053\u305d!!"],objnames:{},filenames:["index"]})

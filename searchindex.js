79e585c32f0c7ac102198472996fb0f349b4b0e4
Tadao Isobe <a1103ti@aiit.ac.jp>
2012-12-15 10:41:51 +0900

    hoge2

--- searchindex.js
+++ searchindex.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 Search.setIndex({objects:{},terms:{"creamy\u3068\u306f":0,creami:0,search:0,index:0,modul:0,indic:0,tabl:0,"\u3078\u3088\u3046\u3053\u305d":0,page:0},objtypes:{},titles:["Creamy \u3078\u3088\u3046\u3053\u305d!!"],objnames:{},filenames:["index"]})
-=======
-Search.setIndex({objects:{},terms:{creami:0,search:0,welcom:0,index:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Creamy Documents&#8217;s documentation!"],objnames:{},filenames:["index"]})
->>>>>>> 0b10ed987bb550e419abc3f498135a0d45e6b302

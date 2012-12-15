51d27fef742ecfbfa203a1032a1652ae49e9f940
Tadao Isobe <a1103ti@aiit.ac.jp>
2012-12-15 12:45:28 +0900

    all delete

--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{"creamy\u3068\u306f":0,creami:0,search:0,index:0,modul:0,indic:0,tabl:0,"\u3078\u3088\u3046\u3053\u305d":0,page:0},objtypes:{},titles:["Creamy \u3078\u3088\u3046\u3053\u305d!!"],objnames:{},filenames:["index"]})

5ca5f3c2a8072b3a9fd0da7a701e7dbf009583cd
Tadao Isobe <a1103ti@aiit.ac.jp>
2013-02-09 16:23:28 +0900

    conflictを削除

--- git_deploy.sh
+++ git_deploy.sh
@@ -1,11 +1,7 @@
 make clean
 make html
-<<<<<<< HEAD
 cp -r ./source/javadoc .
 cp -r ./build/html/* .
-=======
-cp ./build/html/* .
->>>>>>> parent of 2a7e5fe... javadocを追加
 git add .
 git commit -a -m 'commit all'
 git push origin gh-pages
